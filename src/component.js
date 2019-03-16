015ea6d57f22c1efd04cf30a34dd1a5e8f8c5eff
simpart <simpart>
2019-03-16 04:48:15 +0000

    supported config

--- src/component.js
+++ src/component.js
@@ -1,23 +1,48 @@
 /**
- * @file template.js
- * @brief
- * @author 
+ * @file component.js
+ * @brief convert tag to js for component
+ * @author simparts
  */
 let thisobj = null;
 
 let list = [];
 
+let isNumStr = (str) => {
+    try {
+        if ('string' !== typeof str) {
+            return false;
+        }
+        let chk = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        let num = false;
+        for (let sidx=0;sidx < str.length; sidx++) {
+            for (let cidx in chk) {
+                if (str[sidx] === chk[cidx]) {
+                    num = true;
+                    break;
+                }
+            }
+            if (false === num) {
+                return false;
+            }
+            num = false;
+        }
+        return true;
+    } catch (e) {
+        console.error(e.stack);
+        throw e;
+    }
+};
+
 let getValue = (prm) => {
     try {
         let sp_str = prm.split(',');
         if (1 === sp_str.length) {
-            return "'" + sp_str[0] + "'";
-        } else if (("[" === prm[0]) && ("]" === prm[prm.length-1])) {
-            return prm;
+            return (true === isNumStr(sp_str[0])) ? sp_str[0] : "'" + sp_str[0] + "'";
         } else {
             let ret = '[';
             for (let sidx in sp_str) {
-                ret += "'" + sp_str[sidx] + "',";
+                ret += (true === isNumStr(sp_str[0])) ? sp_str[0] : "'" + sp_str[0] + "'";
+                //ret += "'" + sp_str[sidx] + "',";
             }
             return ret + "]";
         }
@@ -54,11 +79,39 @@ try {
                 throw e;
             }
         },
+        config: (prm) => {
+            try {
+                let ret = "";
+                if ( ('effect' !== prm.tag) &&
+                     ('layout' !== prm.tag) &&
+                     ('event'  !== prm.tag) ) {
+                    return ret;
+                }
+                ret += prm.tag + ': [';
+                for (let cidx in prm.child) {
+                    ret += 'new ' + prm.child[cidx].tag + '({' + thisobj.option(prm.child[cidx].attrs) + '}),'
+                }
+                ret += '],';
+                return ret;
+            } catch (e) {
+                console.error(e.stack);
+                throw e;
+            }
+        },
         script: () => {
             try {
                 let ret = '';
                 for (let lidx in list) {
-                    ret += 'new ' + list[lidx].tag + '({'+ thisobj.option(list[lidx].attrs) +'}),'
+                    ret += 'new ' + list[lidx].tag + '({'+ thisobj.option(list[lidx].attrs);
+                    if (0 === list[lidx].child) {
+                        ret += '}),';
+                        continue;
+                    }
+                    for (let cidx in list[lidx].child) {
+                        /* check config */
+                        ret += thisobj.config(list[lidx].child[cidx]);
+                    }
+                    ret += '}),';
                 }
                 return ret;
             } catch (e) {

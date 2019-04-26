0909c82a62c4e00d5d619f3e1c15e25c597abe59
simpart <simpart>
2019-04-26 22:31:36 +0000

    mod page responsive

--- src/conv/Respsv.js
+++ src/conv/Respsv.js
@@ -14,7 +14,7 @@ module.exports = class extends Base {
             this.add(    "document.body.setAttribute('style','margin:0px;padding:0px;font-size:0.16em;');", 2);
             this.add("}");
             this.add("if(null===document.documentElement.getAttribute('style')){");
-            this.add(    "document.documentElement.setAttribute('style','"+ prm +";');", 2);
+            this.add(    "document.documentElement.setAttribute('style','"+ prm +"');", 2);
             this.add("}");
         } catch (e) {
             console.error(e.stack);
@@ -47,6 +47,47 @@ module.exports = class extends Base {
         }
     }
     
+    getTgtValue (prm, idx) {
+        try {
+            let ret     = [];
+            let dup     = false;
+            
+            for (let chk_idx=parseInt(idx)-1; chk_idx >= 0 ;chk_idx--) {
+                if ( (prm[idx].tag !== 'mobule') && (prm[idx].tag !== 'tablet') ) {
+                    dup = true;
+                    break;
+                }
+                if (prm[chk_idx].tag === prm[idx].tag) {
+                    dup = true;
+                    break;
+                }
+            }
+            
+            if ('body' === prm[idx].attrs.target) {
+                if (true === dup) {
+                    ret.push('buf=document.body.getAttribute("style");');
+                    ret.push("mf.func.addHeadStyle(buf+'" + this.getMedia(prm[idx]) + "');");
+                } else {
+                    ret.push('document.body.setAttribute("style","");');
+                    ret.push("mf.func.addHeadStyle('" + this.getMedia(prm[idx]) + "');");
+                }
+            } else {
+                if (true === dup) {
+                    ret.push('buf=document.documentElement.getAttribute("style");');
+                    ret.push("mf.func.addHeadStyle(buf+'" + this.getMedia(prm[idx]) + "');");
+                } else {
+                    ret.push('document.documentElement.setAttribute("style","");');
+                    ret.push("mf.func.addHeadStyle('" + this.getMedia(prm[idx]) + "');");
+                }
+            }
+            
+            return ret;
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
     toScript (prm) {
         try {
             let def_prm = undefined;
@@ -58,27 +99,25 @@ module.exports = class extends Base {
                 }
             }
             this.defStyle(def_prm);
+
+            this.add("let buf=null;");
             let css_val = '';
             for (let pidx in prm) {
-                let fnt_siz = "html{font-size:" + prm[pidx].text + "%;}}";
-                let base    = "@media screen";
+                let tgt_val = this.getTgtValue(prm, pidx);
                 
-                if ( ('mobile' === prm[pidx].tag) ||
-                     ('tablet' === prm[pidx].tag)) {
+                if ( ("mobile" === prm[pidx].tag) ||
+                     ("tablet" === prm[pidx].tag)) {
                     this.add('if ("'+ prm[pidx].tag +'"===mf.func.devType()){');
-                    if ('body' === prm[pidx].attrs.target) {
-                        this.add(    "document.body.setAttribute('style','');", 2);
-                        this.add(    "mf.func.addHeadStyle('" + this.getMedia(prm[pidx]) + "')",2);
-                    } else {
-                        this.add(    "document.documentElement.setAttribute('style','');", 2);
-                        this.add(    "mf.func.addHeadStyle('" + this.getMedia(prm[pidx]) + "')",2);
-                    }
+                    this.add(tgt_val[0], 2);
+                    this.add(tgt_val[1], 2);
                     this.add('}');
-                    
                 } else if ('screen' === prm[pidx].tag) {
                     css_val += this.getMedia(prm[pidx]);
                 } else {
-                    throw new Error('invalid tag:' + prm[pidx].tag);
+                    this.add("if(window.navigator.userAgent.indexOf('" + prm[pidx].tag + "') > 0){");
+                    this.add(tgt_val[0], 2);
+                    this.add(tgt_val[1], 2);
+                    this.add('}');
                 }
             }
             if ('' !== css_val) {
